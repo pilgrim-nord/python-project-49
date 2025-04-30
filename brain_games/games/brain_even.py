db099b2818f178de6ed58bb5b27968d8c71ff628
pilgrim-nord <dmelekhov@gmail.com>
2025-04-30 15:14:07 +0300

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,20 +6,11 @@ MAX_RANDOM_NUMBER = 100
 GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-
-
 def create_task_and_right_answer():
     random_number = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
-    question = f'Question: {random_number}'
-
-    if is_even(random_number):
+    question = f"Question: {random_number}"
+    if random_number % 2 == 0:
         right_answer = 'yes'
     else:
         right_answer = 'no'
-
     return question, right_answer
